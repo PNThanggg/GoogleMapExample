4f761614a51cff79f99fb38aa9ebb1dc24759e4d
PNT <92286791+PNThanggg@users.noreply.github.com>
2023-07-21 03:41:19 +0700

    example

---
+++ android/app/src/main/kotlin/com/pnt/google_map_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pnt.google_map_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:flutter/material.dart';
+
+abstract class GoogleMapExampleAppPage extends StatelessWidget {
+  const GoogleMapExampleAppPage(this.leading, this.title, {super.key});
+
+  final Widget leading;
+  final String title;
+}
